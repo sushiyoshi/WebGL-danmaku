1ad982302b448df95662eb3ce3f385671ae78b32
sushiyoshi <41273823+sushiyoshi@users.noreply.github.com>
2019-01-18 05:48:45 +0900

    Update engine.js

--- engine.js
+++ engine.js
@@ -1,6 +1,3 @@
-// sample_005
-//
-// WebGLでモデル座標変換行列を操作し再帰的にモデルを移動・回転・拡大縮小する
 
 onload = function(){
     // canvasエレメントを取得
@@ -48,6 +45,7 @@ onload = function(){
 
             // 失敗していたらエラーログをアラートする
             alert(gl.getShaderInfoLog(shader));
+
         }
     }
 
@@ -108,6 +106,8 @@ onload = function(){
 
             // attributeLocationを通知し登録する
             gl.vertexAttribPointer(attL[i], attS[i], gl.FLOAT, false, 0, 0);
+
+            gl.bindBuffer(gl.ARRAY_BUFFER, null);
         }
     }
     const ex = (object,object_2) =>  {
@@ -119,7 +119,46 @@ onload = function(){
     }
     const rad = (di) => {
             return di/180*Math.PI;
-      }
+    }
+
+    class drawAll {
+        constructor(array = null,def = null) {
+            this.array = array;
+            this.def = def;
+        }
+        loop() {
+            Object.keys(this.array).forEach((value,index,array) => {
+                exData = this.def;
+                ex(this.array[value],exData);
+                m.identity(mMatrix);
+                exData.speed += exData.acc;
+                exData.dir += exData.dir_sp;
+                exData.edir+=exData.edir_sp;
+                exData.x += Math.cos(exData.dir/180*Math.PI)*exData.speed*0.01;
+                exData.y += Math.sin(exData.dir/180*Math.PI)*exData.speed*0.01;
+                m.multiply(tmpMatrix, mMatrix, mvpMatrix);
+                gl.uniformMatrix4fv(uniLocation[0], false, mvpMatrix);
+                gl.uniform1f(uniLocation[1], exData.size*0.01);
+                gl.uniform2fv(uniLocation[2], [exData.x,exData.y]);
+                gl.uniform1f(uniLocation[3], exData.edir*0.03);
+                gl.uniform1i(uniLocation[4], exData.btype);
+                gl.uniform4fv(uniLocation[5], exData.color);
+                if(exData.btype == 1 || exData.btype == 2) {
+                    gl.drawArrays(gl.TRIANGLES, 0, position[exData.btype].length/2);
+                } else if(exData.btype == 0){
+                    gl.drawArrays(gl.TRIANGLE_FAN, 0, position[exData.btype].length/2);
+                    gl.uniform1f(uniLocation[1], exData.size*0.007);
+                    gl.uniform4fv(uniLocation[5], [1.0,1.0,1.0,0.0]);
+                    gl.drawArrays(gl.TRIANGLE_FAN, 0, position[exData.btype].length/2);
+                }
+                
+                if(Math.abs(exData.x) > 3.5 || Math.abs(exData.y) > 2) {
+                    delete_.push(value);
+                }
+                this.array[value] = JSON.parse(JSON.stringify(exData));
+            });
+        }
+    }
     // webglコンテキストを取得
     let gl = c.getContext('webgl') || c.getContext('experimental-webgl');
 
@@ -129,24 +168,8 @@ onload = function(){
 
     // プログラムオブジェクトの生成とリンク
     let prg = create_program(v_shader, f_shader);
-
-    // attributeLocationを配列に取得
     let attLocation = new Array();
-    attLocation[0] = gl.getAttribLocation(prg, 'pos0');
-    attLocation[1] = gl.getAttribLocation(prg, 'col0');
-    attLocation[2] = gl.getAttribLocation(prg, 'pos1');
-    attLocation[3] = gl.getAttribLocation(prg, 'col1');
-    attLocation[4] = gl.getAttribLocation(prg, 'pos2');
-    attLocation[5] = gl.getAttribLocation(prg, 'col2');
-
-    // attributeの要素数を配列に格納
     let attStride = new Array(2);
-    attStride[0] = 2;
-    attStride[1] = 4;
-    attStride[2] = 2;
-    attStride[3] = 4;
-    attStride[4] = 2;
-    attStride[5] = 4;
 
     // 頂点属性を格納する配列
     let position = [];
@@ -166,15 +189,38 @@ onload = function(){
         position[1].push(0,0);
         color[1].push(0.0,0.0,0.0,0.0);
     }
+    position[2] =[];
+    color[2] =[];
+    for(let i = 0; i < 36; i++) {
+        position[2].push(Math.cos(Math.PI/180*i*10),Math.sin(Math.PI/180*i*10));
+        color[2].push(0.0,0.5,0.8,0.5);
+        
+        position[2].push(0,0);
+        color[2].push(0.0,0.0,0.0,0.0);
+    }
+    /*
+    position[3] =[];
+    color[3] =[];
+    for(let i = 5; i < 15; i++) {
+        position[3].push(Math.cos(Math.PI/180*i*10 + 0.1) ,Math.sin(Math.PI/180*i*10));
+        color[3].push(0.0,0.5,0.8,0.5);
+    }
+    
+    for(let i = 16; i < 36; i++) {
+        position[3].push(Math.cos(Math.PI/180*i*10 - 0.1) ,Math.sin(Math.PI/180*i*10));
+        color[3].push(0.0,0.5,0.8,0.5);
+    }*/
     // VBOの生成
-    let vbo_obj = [];
     position.forEach((v,i) => {
+        vbo_obj = [];
         vbo_obj.push( create_vbo(position[i]) );
         vbo_obj.push( create_vbo(color[i]) );
+        attLocation[0] = gl.getAttribLocation(prg, `pos${i}`);
+        attLocation[1] = gl.getAttribLocation(prg, `col${i}`);
+        attStride[0] = 2;
+        attStride[1] = 4;
+        set_attribute(vbo_obj, attLocation, attStride);
     })
-
-    // VBO を登録する
-    set_attribute(vbo_obj, attLocation, attStride);
 
     // uniformLocationの取得
     let uniLocation = new Array();
@@ -208,31 +254,39 @@ onload = function(){
         e++;
         for(i = 0; i < 10; i++) {
             n++;
-            obj[n] = ({dir:e*10 + i*36,acc:0.01,speed:0.1,size:3,btype:1,color:[-0.5,-0.3,0.8,-0.5]})
+            obj[n] = ({x:Math.cos(n*0.01)*0.4,y:Math.sin(n*0.01)*0.4,dir:e*10 + i*36,acc:0.01,speed:0.1,size:4,btype:2})
         }
-    } ,100)
+    } ,150)
 
     setInterval(() => {
         d-=10;
         for(i = 0; i < 10; i++) {
             n++;
-            obj[n] = ({dir:d + i*-36,acc:0.02,speed:0,btype:1})
+            obj[n] = ({x:Math.sin(n*0.02)*0.4,y:Math.cos(n*0.02)*0.4,dir:d + i*-36,acc:0.02,speed:0,btype:1,dir_ap:1})
         }
-    } ,200)
+    } ,250)
+
+    setInterval(() => {
+        for(i = 0; i < 10; i++) {
+            n++;
+            obj[n] = ({dir:e*10 + i*36,acc:0.004,speed:0.5,size:6,color:[0.5,0.0,0.2,0.0]})
+        }
+    } ,1000)
     let count = 0;
     let delete_ = [];
     let exData
     // 恒常ループ
+
     (function(){
 
         // canvasを初期化
         gl.clearColor(0.0, 0.0, 0.0, 1.0);
         gl.clearDepth(1.0);
         gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-        console.log(Object.keys(obj).length)
         Object.keys(obj).forEach((value,index,array) => {
             exData = {
                 dir:0,
+                dir_sp:0,
                 edir:0,
                 edir_sp:1,
                 x:0,
@@ -241,16 +295,15 @@ onload = function(){
                 acc:0,
                 size:5,
                 btype:0,
-                color:[1.0,0.0,0.0,0.0]
+                color:[0.0,0.0,0.0,0.0]
             }
             ex(obj[value],exData);
             m.identity(mMatrix);
             exData.speed += exData.acc;
+            exData.dir += exData.dir_sp;
             exData.edir+=exData.edir_sp;
             exData.x += Math.cos(exData.dir/180*Math.PI)*exData.speed*0.01;
             exData.y += Math.sin(exData.dir/180*Math.PI)*exData.speed*0.01;
-            //m.translate(mMatrix,[Math.cos(rad(exData.edir))*Math.sin(rad(exData.edir))*size*-1,Math.sin(rad(exData.edir))*Math.cos(rad(exData.edir)),0],mMatrix);
-            //m.rotate(mMatrix, exData.edir*0.01, [exData.x+Math.cos(rad(exData.edir)), exData.y-Math.sin(rad(exData.edir)), 0], mMatrix);
             m.multiply(tmpMatrix, mMatrix, mvpMatrix);
             gl.uniformMatrix4fv(uniLocation[0], false, mvpMatrix);
             gl.uniform1f(uniLocation[1], exData.size*0.01);
@@ -258,16 +311,13 @@ onload = function(){
             gl.uniform1f(uniLocation[3], exData.edir*0.03);
             gl.uniform1i(uniLocation[4], exData.btype);
             gl.uniform4fv(uniLocation[5], exData.color);
-            switch(exData.btype) {
-                case 1:
+            if(exData.btype == 1 || exData.btype == 2) {
                 gl.drawArrays(gl.TRIANGLES, 0, position[exData.btype].length/2);
-                break;
-                default :
+            } else if(exData.btype == 0){
                 gl.drawArrays(gl.TRIANGLE_FAN, 0, position[exData.btype].length/2);
                 gl.uniform1f(uniLocation[1], exData.size*0.007);
                 gl.uniform4fv(uniLocation[5], [1.0,1.0,1.0,0.0]);
                 gl.drawArrays(gl.TRIANGLE_FAN, 0, position[exData.btype].length/2);
-                break;
             }
             
             if(Math.abs(exData.x) > 3.5 || Math.abs(exData.y) > 2) {
@@ -277,7 +327,6 @@ onload = function(){
         })
         delete_.forEach((value,index,array) => {
             delete obj[value];
-            console.log('delete');
         })
         delete_ = [];
         // コンテキストの再描画
